371cec576264c10341c3c8e3d58ea7e10533b438
Adrien Emery <adrien.emery@gmail.com>
2015-11-22 08:54:23 +0000

    Update ports and pins

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ manager.create_api(Position, methods=['GET', 'POST', 'PATCH', 'DELETE'])
 
 if __name__ == '__main__':
     # start the flask app
-    app.run(debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=8000)
